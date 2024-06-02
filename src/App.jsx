0174ace03979f9e5c68f61c01129bd07f99df1ee
Ruchi <ruchi.thanabhawan@gmail.com>
2024-06-02 10:08:52 -0700

    setup initial layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 //import { useState } from "react";
 
+import Dashboard from "./Dashboard";
+import Drawer from "./Drawer";
+import Navbar from "./Navbar";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Navbar />
+      <div className="drawer lg:drawer-open">
+        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col items-center justify-center">
+          {/* Page content here */}
+          <Dashboard />
+        </div>
+        <Drawer />
+      </div>
     </>
   );
 }

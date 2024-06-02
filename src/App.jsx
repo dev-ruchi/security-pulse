1a1d5b7930c10f1dceaef65891c7ea8289a85313
Ruchi <ruchi.thanabhawan@gmail.com>
2024-06-02 14:40:10 -0700

    fetch data from server

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,23 @@ import Drawer from "./Drawer";
 import Navbar from "./Navbar";
 
 function App() {
+  fetch("http://localhost:3000/events")
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
+
   return (
     <>
       <Navbar />
+      <h1>Welcome to The Dashboard</h1>
       <div className="drawer lg:drawer-open">
         <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content flex flex-col items-center justify-center">
           {/* Page content here */}
+
           <Dashboard />
         </div>
         <Drawer />
